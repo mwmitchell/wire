2cfa809ec501005ea5630877d051b62929655403
Matt Mitchell <mmitchell@roomkey.com>
2013-09-23 20:33:00 -0400

    Updating project info

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject wire "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject com.codesignals/wire "0.1.0-SNAPSHOT"
+  :description "A routing lib for Clojure and Ring"
+  :url "http://github.com/mwmitchell/wire"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
