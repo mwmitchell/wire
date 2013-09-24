5be33d740934798f60f0c76d2d89862aca0df8fe
Matt Mitchell <mmitchell@roomkey.com>
2013-09-23 21:12:51 -0400

    minor cleanup

--- src/wire/generate.clj
+++ src/wire/generate.clj
@@ -1,15 +1,14 @@
 (ns wire.generate
   (:require [clojure.string :as s]
-            [clojure.set :as set]))
+            [clojure.set :refer [difference]]))
 
 (defn find-by-id [route-defs id]
   (or (some #(and (= (:id %) id) %) route-defs)
       (throw (Exception. (format "Route not found: %s" id)))))
 
 (defn path-for [route-defs id opts]
-  (let [{:keys [path]} (find-by-id route-defs id)
-        pkeys (map (comp keyword last) (re-seq #":([^ \/]+)" path))
-        diff (seq (set/difference (set pkeys) (-> opts keys set)))]
-    (when diff
-      (throw (Exception. (format "The following params are missing: %s" diff))))
-    (reduce (fn [m [k v]] (s/replace m (str "/" k) (str "/" v))) path opts)))
+  (let [{p :path} (find-by-id route-defs id)
+        pkeys (map (comp keyword last) (re-seq #":([^ \/]+)" p))]
+    (when-let [diff (seq (difference (set pkeys) (-> opts keys set)))]
+      (throw (Exception. (format "The route params for %s are missing: %s" id diff))))
+    (reduce-kv #(s/replace %1 (str "/" %2) (str "/" %3)) p opts)))
