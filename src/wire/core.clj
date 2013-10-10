2b3a240e2c14a6a5ae0502f7fd10a92c50b3c9f5
Matt Mitchell <mmitchell@roomkey.com>
2013-10-10 19:06:29 -0400

    Refactoring core fns

--- src/wire/core.clj
+++ src/wire/core.clj
@@ -2,25 +2,32 @@
   (:require [clojure.string :as s]
             [clojure.set :refer [difference]]))
 
+;; A Routes is a map:
+;; {:name :keyword-identifier
+;;  :path "the-path" ;; also ["path/:var" :var #"regexp-rule"]
+;;  :methods {:get fn1 :post fn2}
+;;  :routes []}
+
 (def http-methods #{:get :post :put :delete :patch :head :options :trace :connect})
 
-(defn- parse-path [{v :path}]
-  (cond
-   (string? v) {:path v :rules {}}
-   (vector? v) {:path (first v) :rules (apply hash-map (rest v))}
-   :else (throw (Exception. "Path must be a string or vector"))))
+(defn- merge-path-attrs [{v :path :as m}]
+  (merge m (cond
+            (string? v) {:path v :rules {}}
+            (vector? v) {:path (first v) :rules (apply hash-map (rest v))}
+            :else (throw (Exception. "Path must be a string or vector")))))
 
 (defn route
   "Builds a tree of routes from:
-   [:name opts-map child1 child2 child3 ...]"
+   [:name opts-map child1 child2 child3 ...]
+  If a :path is not provided, the id of the route is used."
   [id opts & children]
   {:pre [(map? opts)]}
-  (let [r (merge {:name id
-                  :path (when id (name id))
-                  :methods (select-keys opts (conj http-methods :any))
-                  :routes (mapv #(apply route %) children)}
-                 (apply dissoc opts http-methods))]
-    (merge r (parse-path r))))
+  (-> {:name id
+       :path (when id (name id))
+       :methods (select-keys opts (conj http-methods :any))
+       :routes (mapv #(apply route %) children)}
+      (merge (apply dissoc opts http-methods))
+      (merge-path-attrs)))
 
 (defn routes
   "Builds a set of routes with a base root node"
