496333b6a75a6bb09eee2ccce81566d7967fcdbe
Matt Mitchell <mmitchell@roomkey.com>
2013-10-09 20:53:00 -0400

    Simplifying `routes` function

--- src/wire/core.clj
+++ src/wire/core.clj
@@ -10,24 +10,19 @@
    (vector? v) {:path (first v) :rules (apply hash-map (rest v))}
    :else (throw (Exception. "Path must be a string or vector"))))
 
-(defn root
-  "Creates a root container and sets path to an empty string"
-  [opts & children]
-  {:pre [(map? opts)]}
-  `[nil ~(merge {:path ""} opts) ~@children])
-
 (defn route
   "Builds a tree of routes from:
-   [:name opts-map child ...]"
-  [[id opts & children]]
+   [:name opts-map child1 child2 child3 ...]"
+  [id opts & children]
+  {:pre [(map? opts)]}
   (let [r (merge {:name id
-                  :path (if id (name id) nil)
+                  :path (when id (name id))
                   :methods (select-keys opts (conj http-methods :any))
-                  :routes (map route children)}
+                  :routes (mapv #(apply route %) children)}
                  (apply dissoc opts http-methods))]
     (merge r (parse-path r))))
 
 (defn routes
   "Builds a set of routes with a base root node"
   [root-opts & children]
-  (route (apply root root-opts children)))
+  (apply route nil (merge {:path ""} root-opts) children))
