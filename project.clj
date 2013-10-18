f40bb1f8a7bab9c3780b58cbaa7002cb52a3f0c6
Matt Mitchell <mmitchell@roomkey.com>
2013-10-18 12:53:47 -0400

    Wrapping compojure.response/render
    * At some point, wire may just import the code like it was prior to this commit
    * Bumped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.codesignals/wire "0.3.1"
+(defproject com.codesignals/wire "0.3.2"
   :description "A routing lib for Clojure and Ring"
   :url "http://github.com/mwmitchell/wire"
   :license {:name "Eclipse Public License"
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clout "1.1.0"]
                  [ring/ring-core "1.2.0"]
+                 [compojure "1.1.5"]
                  [org.clojure/tools.logging "0.2.6"]])
