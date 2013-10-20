aa28adb6b1c5f8cef64df077c1243a403dbf6b69
Matt Mitchell <mmitchell@roomkey.com>
2013-10-20 17:31:15 -0400

    Bumped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.codesignals/wire "0.3.2"
+(defproject com.codesignals/wire "0.3.3"
   :description "A routing lib for Clojure and Ring"
   :url "http://github.com/mwmitchell/wire"
   :license {:name "Eclipse Public License"
