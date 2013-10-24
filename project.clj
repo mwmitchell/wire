6a7e818442812441f564c1305e924f4a58ef4ad3
Matt Mitchell <mmitchell@roomkey.com>
2013-10-23 21:32:23 -0400

    Bumped version, removed ring dep

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject com.codesignals/wire "0.4.0"
+(defproject com.codesignals/wire "0.4.1"
   :description "A routing lib for Clojure and Ring"
   :url "http://github.com/mwmitchell/wire"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clout "1.1.0"]
-                 [ring/ring-core "1.2.0"]
                  [compojure "1.1.5"]
                  [org.clojure/tools.logging "0.2.6"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
